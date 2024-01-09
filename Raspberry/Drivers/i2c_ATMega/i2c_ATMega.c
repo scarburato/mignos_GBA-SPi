29a3622472e4c5983a1bf1bf51f89a1e64e5e161
dario <dariopag45@gmail.com>
2024-01-09 18:47:45 +0100

    input handling

--- Raspberry/Drivers/i2c_ATMega/i2c_ATMega.c
+++ Raspberry/Drivers/i2c_ATMega/i2c_ATMega.c
@@ -1,4 +1,3 @@
-#include "linux/bits.h"
 #include <linux/module.h>
 #include <linux/reboot.h>
 #include <linux/kernel.h>
@@ -17,12 +16,13 @@ static struct i2c_adapter *etx_i2c_adapter = NULL;           // I2C Adapter Stru
 static struct i2c_client *etx_i2c_client_ATMega_GBASPi = NULL; // I2C Cient Structure (In our case it is OLED)
 static struct sys_off_handler *poweroff_handler = NULL;
 static struct input_dev *gamepad_idev = NULL;
+static struct input_dev *pwr_btn_idev = NULL;
 
 //  Writes data into the I2C client
 //   Arguments:
 //       buff -> buffer to be sent
 //       len  -> Length of the data
-static int I2C_Write(unsigned char *buf, unsigned int len)
+static int i2c_write(unsigned char *buf, unsigned int len)
 {
     return i2c_master_send(etx_i2c_client_ATMega_GBASPi, buf, len);
 }
@@ -31,7 +31,7 @@ static int I2C_Write(unsigned char *buf, unsigned int len)
 //  Arguments:
 //      out_buff -> buffer wherer the data to be copied
 //      len      -> Length of the data to be read
-static int I2C_Read(unsigned char *out_buf, unsigned int len)
+static int i2c_read(unsigned char *out_buf, unsigned int len)
 {
     return i2c_master_recv(etx_i2c_client_ATMega_GBASPi, out_buf, len);
 }
@@ -44,7 +44,7 @@ static void ATMega_GBASPi_Write(bool led)
     } else { 
         buff = 0x0; 
     } 
-    I2C_Write(&buff, sizeof(buff));
+    i2c_write(&buff, sizeof(buff));
 }
 
 // poweroff handler
@@ -54,6 +54,39 @@ static int ATMega_GBASPi_poweroff(struct sys_off_data *data)
     ATMega_GBASPi_Write(0);
     mdelay(200);
     return 0;
+}
+
+/*
+ * Threaded IRQ handler and this can (and will) sleep.
+ */
+static irqreturn_t atmega_gbaspi_irq_handler(int irq, void *dev_id)
+{
+    int error;
+    uint16_t packet = 0; // this won't work on big endian machines
+
+    error = i2c_read((void*)&packet, 2);
+    if(error)
+        return IRQ_HANDLED;
+
+    // Update buttons
+    input_report_key(gamepad_idev, BTN_A, packet & 0b10 << 0);
+    input_report_key(gamepad_idev, BTN_B, packet & 0b10 << 1);
+    input_report_key(gamepad_idev, BTN_TL, packet & 0b10 << 6);
+    input_report_key(gamepad_idev, BTN_TR, packet & 0b10 << 7);
+
+    // POV
+    // @TODO check values
+    input_report_abs(gamepad_idev, ABS_HAT0X, packet & 0b10 << 4 ? -1 : packet & 0b10 << 5 ? 1 : 0);
+    input_report_abs(gamepad_idev, ABS_HAT0Y, packet & 0b10 << 2 ? -1 : packet & 0b10 << 3 ? 1 : 0);
+
+    // Update pwr btn
+    input_report_key(pwr_btn_idev, KEY_POWER, packet & 0x01);
+
+    // report new state
+    input_sync(gamepad_idev);
+    input_sync(pwr_btn_idev);
+
+    return IRQ_HANDLED;
 }
 
 // This function getting called when the slave has been found
@@ -121,9 +154,12 @@ static int __init etx_driver_init(void)
     i2c_add_driver(&etx_ATMega_GBASPi_driver);    
     i2c_put_adapter(etx_i2c_adapter);
 
-    pr_info("Driver Added!!!\n");
+    ret = devm_request_threaded_irq(&etx_i2c_client_ATMega_GBASPi->dev, etx_i2c_client_ATMega_GBASPi->irq,
+					  NULL, atmega_gbaspi_irq_handler,
+					  IRQF_SHARED | IRQF_ONESHOT,
+					  etx_i2c_client_ATMega_GBASPi->name, NULL);
 
-    poweroff_handler = register_sys_off_handler(SYS_OFF_MODE_POWER_OFF_PREPARE,0,ATMega_GBASPi_poweroff,NULL);
+    poweroff_handler = register_sys_off_handler(SYS_OFF_MODE_POWER_OFF_PREPARE, 0, ATMega_GBASPi_poweroff, NULL);
     if(IS_ERR_VALUE(poweroff_handler))
     {
         printk(KERN_WARNING "Unable to register poweroff handler, the system will not automatically cut power\n");
@@ -145,29 +181,58 @@ static int __init etx_driver_init(void)
     gamepad_idev->id.vendor = 0xcafe;
     gamepad_idev->id.product = 0xbabe; // @todo capire
     gamepad_idev->id.version = 1;
-
-    // Set up supported keys and axes
-    gamepad_idev->evbit[0] = BIT_MASK(EV_KEY) | BIT_MASK(EV_ABS);
-    gamepad_idev->keybit[BIT_WORD(BTN_A)] = BIT_MASK(BTN_A);
-    gamepad_idev->keybit[BIT_WORD(BTN_B)] = BIT_MASK(BTN_B);
-    gamepad_idev->keybit[BIT_WORD(BTN_TL)] = BIT_MASK(BTN_TL);
-    gamepad_idev->keybit[BIT_WORD(BTN_TR)] = BIT_MASK(BTN_TR);
-
-    // The power button
-    gamepad_idev->keybit[BIT_WORD(KEY_POWER)] = BIT_MASK(KEY_POWER);
+    gamepad_idev->dev.parent = &etx_i2c_client_ATMega_GBASPi->dev;
 
     // Set up POV-HAT
     input_set_abs_params(gamepad_idev, ABS_HAT0X, -1, 1, 0, 0);
     input_set_abs_params(gamepad_idev, ABS_HAT0Y, -1, 1, 0, 0);
 
+    // Set up supported keys and axes
+    gamepad_idev->evbit[0] = BIT_MASK(EV_KEY) | BIT_MASK(EV_ABS);
+    set_bit(EV_ABS, gamepad_idev->evbit);
+    set_bit(BTN_A, gamepad_idev->keybit);
+    set_bit(BTN_B, gamepad_idev->keybit);
+    set_bit(BTN_TL, gamepad_idev->keybit);
+    set_bit(BTN_TR, gamepad_idev->keybit);
+
+    // The power button
+    pwr_btn_idev = input_allocate_device();
+    if(pwr_btn_idev == NULL)
+    {
+        ret = ENOMEM;
+        goto err1;
+    }
+
+    // Set up its params
+    pwr_btn_idev->name = "Gameboy sys controls";
+    pwr_btn_idev->id.bustype = BUS_I2C;
+    pwr_btn_idev->id.vendor = 0xcafe;
+    pwr_btn_idev->id.product = 0xbabe + 1; // @todo capire
+    pwr_btn_idev->id.version = 1;
+    pwr_btn_idev->dev.parent = &etx_i2c_client_ATMega_GBASPi->dev;
+
+    pwr_btn_idev->evbit[0] = BIT_MASK(EV_KEY);
+    set_bit(KEY_POWER, pwr_btn_idev->keybit);
+
 
     if(input_register_device(gamepad_idev))
     {
         ret = EINVAL;
-        goto err1;
+        goto err2;
+    }
+
+    if(input_register_device(pwr_btn_idev))
+    {
+        ret = EINVAL;
+        goto err3;
     }
 
     return 0;
+
+err3:
+    input_unregister_device(gamepad_idev);
+err2:
+    input_free_device(pwr_btn_idev);
 err1:
     input_free_device(gamepad_idev);
 err0:
@@ -183,7 +248,9 @@ err0:
 // Module Exit function
 static void __exit etx_driver_exit(void)
 {
+    input_unregister_device(pwr_btn_idev);
     input_unregister_device(gamepad_idev);
+    input_free_device(pwr_btn_idev);
     input_free_device(gamepad_idev);
 
     i2c_unregister_device(etx_i2c_client_ATMega_GBASPi);
